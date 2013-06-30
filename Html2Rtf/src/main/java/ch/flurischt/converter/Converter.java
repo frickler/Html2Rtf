d80e133d23b411ddcf4d578ec92c6db6952baf2f
Flurin Rindisbacher <info@flurischt.ch>
2013-06-30 14:14:08 +0200

    add a simple cli for manual tests
    use maven package and then java -jar
    added commons-io and commons-cli dependencies

--- Html2Rtf/src/main/java/ch/flurischt/converter/Converter.java
+++ Html2Rtf/src/main/java/ch/flurischt/converter/Converter.java
@@ -1,7 +1,18 @@
 package ch.flurischt.converter;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.io.StringWriter;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.io.FileUtils;
 
 import ch.flurischt.html2rtf.Html2RtfParser;
 import ch.flurischt.rtf2html.Rtf2Html;
@@ -16,13 +27,16 @@ import com.tutego.jrtf.Rtf;
  */
 public class Converter {
 
+	private static final String CONVERT_MODE_HTML2RTF = "h2r";
+	private static final String CONVERT_MODE_RTF2HTML = "r2h";
+
 	/**
 	 * Converts a HTML-Tree to RTF
 	 * 
 	 * @param html
 	 * 
 	 * @return
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static String toRtf(String html) throws IOException {
 		Rtf rtf = new Html2RtfParser().parse(html);
@@ -42,4 +56,75 @@ public class Converter {
 		return new Rtf2Html().toHtml(rtf);
 	}
 
+	public static void main(String[] args) {
+		try {
+			parse(args);
+		} catch (ParseException exp) {
+			// TODO ?
+			System.err.println("Unexpected exception:" + exp.getMessage());
+		} catch (IOException e) {
+			System.err.println("Could not open/write File: " + e.getMessage());
+		}
+	}
+
+	private static void parse(String[] args) throws ParseException, IOException {
+		CommandLineParser parser = new GnuParser();
+		Options options = new Options();
+		options.addOption("i", "in", true, "path to input file");
+		options.addOption("o", "out", true, "path to output file");
+		options.addOption("m", "mode", true, "Mode: h2r (html to rtf) or r2h");
+
+		CommandLine line = parser.parse(options, args);
+
+		// validate that block-size has been set
+		if (line.hasOption("block-size")) {
+			// print the value of block-size
+			System.out.println(line.getOptionValue("block-size"));
+		}
+
+		String inFile = null;
+		String outFile = null;
+		String mode = null;
+
+		if (line.hasOption("in"))
+			inFile = line.getOptionValue("in");
+		if (line.hasOption("out"))
+			outFile = line.getOptionValue("out");
+		if (line.hasOption("mode"))
+			mode = line.getOptionValue("mode");
+
+		if ((!CONVERT_MODE_HTML2RTF.equals(mode) && !CONVERT_MODE_RTF2HTML
+				.equals(mode)) || inFile == null || outFile == null) {
+			printHelp(
+					options,
+					"parse a \"well\" formed html page into a rtf document and vice versa\n",
+					"\n", System.out);
+			return;
+		}
+
+		convert(mode, inFile, outFile);
+	}
+
+	private static void printHelp(final Options options, String header,
+			final String footer, final OutputStream out) {
+		final PrintWriter writer = new PrintWriter(out);
+		final HelpFormatter helpFormatter = new HelpFormatter();
+		helpFormatter.printHelp(writer, 80, "java -jar xyz.jar", header,
+				options, 3, 5, footer, true);
+		writer.close();
+	}
+
+	private static void convert(String mode, String inFilePath,
+			String outFilePath) throws IOException {
+
+		String input = FileUtils.readFileToString(new File(inFilePath));
+		String out = null;
+
+		if (CONVERT_MODE_HTML2RTF.equals(mode)) {
+			out = Converter.toRtf(input);
+		} else if (CONVERT_MODE_RTF2HTML.equals(mode)) {
+			out = Converter.toHtml(input);
+		}
+		FileUtils.write(new File(outFilePath), out);
+	}
 }
